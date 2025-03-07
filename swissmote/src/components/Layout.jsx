96b19b6ce93d76d5e629875534a1810dc99d3687
Swaminathanjk <swaminathanjk@gmail.com>
2025-03-07 10:17:27 +0530

    fixed_layout

--- swissmote/src/components/Layout.jsx
+++ swissmote/src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import "../styles/layout.css";
 const Layout = ({ children }) => {
   return (
     <div className="layout">
